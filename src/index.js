6c0089e14dfe4ba103d811bf1ee17a0a1721eb00
Shane Gibbons <shane.gibbons@gmail.com>
2019-01-04 02:17:17 -0600

    Fix index to use old style requires
    Was breaking on the new style. Not worth chasing right now.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import { createServer } from 'http';
-import { hostname } from 'os';
-import { message } from '../config/config.json';
+const http = require('http');
+const os = require('os');
+const config = require('../config/config.json');
 
-const server = createServer((request, response) => {
+const server = http.createServer((request, response) => {
   response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.end(`${message} on host ${hostname()}\n`);
+  response.end(`${config.message} on host ${os.hostname()}\n`);
 });
 
 const port = 80;
