1de82b4ffa28216c80e8237ac86a34938e26597c
Shane Gibbons <shane.gibbons@gmail.com>
2019-01-06 19:17:16 -0600

    Remove copy-build-logs.sh
    The commands are built into the makefile.

--- abin/copy-build-logs.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-# since the application build is running as part of the docker build,
-# we want to pull out the build logs so that we can store them in the artifact repository.
-
-docker create --name double-tap-copy-logs double-tap:build
-docker cp double-tap-copy-logs:/app/build_logs .
-docker rm -v double-tap-copy-logs
