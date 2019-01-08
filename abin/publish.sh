da34d8ab34f0c184a96a14db4c240c958e945a60
Shane Gibbons <shane.gibbons@gmail.com>
2019-01-07 20:25:59 -0600

    Publish the test image

--- abin/publish.sh
+++ abin/publish.sh
@@ -8,20 +8,20 @@ set -eo pipefail
 # though the versioned package will be created in the publish image
 source abin/ensure-version-vars.sh
 
-echo "Tagging..."
+echo "Tagging and publishing images..."
+
 CONTAINER_NAME=double-tap
 REPOSITORY=dshaneg
 APP_TAG=${REPOSITORY}/${CONTAINER_NAME}:${APP_VERSION}
 DEPLOY_TAG=${APP_TAG}.deploy
+TEST_TAG=${APP_TAG}.test
 
-echo "Tagging deploy image with ${DEPLOY_TAG}"
+set -x
+
 docker tag ${CONTAINER_NAME}:deploy ${DEPLOY_TAG}
-
-echo "Tagging app image with ${APP_TAG}"
+docker tag ${CONTAINER_NAME}:test ${TEST_TAG}
 docker tag ${CONTAINER_NAME}:latest ${APP_TAG}
 
-echo "Pushing ${DEPLOY_TAG}"
 docker push ${DEPLOY_TAG}
-
-echo "Pushing ${APP_TAG}"
+docker push ${TEST_TAG}
 docker push ${APP_TAG}
